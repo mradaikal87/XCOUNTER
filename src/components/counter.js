4268b5edac6cbb9b807e79f372b9c4cff0fcfd69
anand <mranand.cse@gmail.com>
2025-08-06 22:52:28 +0530

    counter app code upload

---
+++ src/components/counter.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import styles from "./counter.module.css";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <section className={styles.counterSection}>
+      <div>
+        <h1>Counter App</h1>
+        <h5>Count:{count}</h5>
+        <div>
+          <button onClick={() => setCount(count + 1)}>Increment</button>
+          <button onClick={() => setCount(count - 1)}>Decrement</button>
+        </div>
+      </div>
+    </section>
+  );
+}
